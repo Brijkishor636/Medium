bb1bd6f1b1092615724f43d5bbc9830f9f008cbb
Brijkishor636 <kbrijkishor636@gmail.com>
2025-04-22 00:48:15 +0530

    Finallised

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,9 +19,3 @@ app.route("/api/v1/blog", blogRouter);
 
 
 export default app
-
-// postgres://avnadmin:AVNS_yJJiDbFr4dgtX3Ui6Nq@pg-cbc4675-brijkishor6206971311-8a84.k.aivencloud.com:15567/defaultdb?sslmode=require
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjd9.XoJa_kYElCkItvYDE3WukwL9IiGy90JAh8yYnZH_H1k
-
-// DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZDJmMTZkZDktNWQ3Mi00MmU1LWExYjgtNGQ0NDNjYjUwNDFmIiwidGVuYW50X2lkIjoiMDUyNjBmYjJjNDIyZTdlNWJmMmMzNjk2MzQzYzQ5NmU2NzJhZTUwNWEyZTU0NTFjYTQ2YmI3NTZkMzhiNWZiNiIsImludGVybmFsX3NlY3JldCI6ImJiZDU0NWU1LWZmMmMtNDg2NS04YzRmLTQyNzhiY2E0M2MyMiJ9.ILVTmBYyVlwQhrtkGqg5dhThcyoZqbzssRzYsI5W374"
